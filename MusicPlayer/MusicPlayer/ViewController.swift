15eebcb892cb437f7aee666ec47f557be89597d5
ouibene <ouibene@gmail.com>
2020-11-22 19:40:26 +0900

    first commit

---
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by ye on 2020/11/22.
+//  Copyright © 2020 oui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
