7f8da235a7ec4b1c7a424fa35fded5140456038e
ouibene <ouibene@gmail.com>
2020-11-23 23:23:51 +0900

    Add objects to the user interface

--- MusicPlayer/MusicPlayer/ViewController.swift
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var progressSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func touchUpPlayPauseButton (_ sender : UIButton) {
+        
+        print("button tapped")
+        
+    }
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        
+        print("slider value changed")
+    }
 }
 
